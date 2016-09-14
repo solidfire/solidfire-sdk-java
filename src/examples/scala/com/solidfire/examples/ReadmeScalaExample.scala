ec5dc981d25a70043bd00ff8009136223dabb988
Jason Ryan Womack <jason.womack@solidfire.com>
2016-09-14 09:56:48 -0600

    Updating example code to match README

--- src/examples/scala/com/solidfire/examples/ReadmeScalaExample.scala
+++ src/examples/scala/com/solidfire/examples/ReadmeScalaExample.scala
@@ -1,36 +1,45 @@
-package com.solidfire.examples
-
-// Import your Java Primitive Types
 import com.solidfire.client.ElementFactory
 import com.solidfire.element.api._
 import com.solidfire.jsvcgen.javautil.Optional.{empty, of}
 
 class ReadmeScalaExample {
 
-
   // Create Connection to SF Cluster
-  val sf = ElementFactory.create( "mvip", "username", "password", "8.0" )
+  val sfe = ElementFactory.create( "mvip", "username", "password", "8.0" )
 
-  // Create some accounts
+  //* --------- EXAMPLE 1 - CREATE AN ACCOUNT ----------- *//
+  // Construct an "AddAccount" request with only required parameters using the builder
   val addAccount = AddAccountRequest.builder.username( "username" ).build
-  val accountId  = sf.addAccount( addAccount ).getAccountID
 
-  // And a volume
-  val createVolume = new CreateVolumeRequest( "volumeName", accountId, 1000000000L, false, empty[QoS], empty( ) )
-  val volumeId     = sf.createVolume( createVolume ).getVolumeID
+  // Send the request and pull the account ID from the result object
+  val accountId = sfe.addAccount( addAccount ).getAccountID
 
-  // Lookup iqn for new volume
-  val iqn: String = sf.listVolumesForAccount( accountId, of( volumeId ), of( 1L ) ).getVolumes( )( 0 ).getIqn
+  //* --------- EXAMPLE 2 - CREATE A VOLUME ------------- *//
+  // Construct a "CreateVolume" request with parameters using the constructor.
+  val createVolume = new CreateVolumeRequest( "volumeName", accountId,
+    1000000000L, false, empty[QoS], empty( ) )
 
+  // Send the request pull the VolumeID off the result object
+  val volumeId = sfe.createVolume( createVolume ).getVolumeID
+
+  //* --------- EXAMPLE 3 - LIST ONE VOLUME FOR AN ACCOUNT ------------- *//
+  // Send the "ListVolume" request with desired parameters inline and pull the first volume in the result
+  val volume: Volume = sfe.listVolumesForAccount( accountId, of( volumeId ), of( 1L ) ).getVolumes( 0 )
+
+  // pull the iqn from the volume
+  val iqn: String = volume.getIqn
+
+  //* --------- EXAMPLE 4 - MODIFY A VOLUME ------------- *//
   // Change Min and Burst QoS while keeping Max and Burst Time the same
   val qos: QoS = new QoS( of( 5000l ), empty(), of( 30000l ), empty() )
 
-  // Modify the volume
+  // Construct request to modify the volume size and QoS using the builder.
   val modifyVolume = ModifyVolumeRequest.builder
     .volumeID( volumeId )
     .optionalQos( qos )
     .optionalTotalSize( 2000000000l )
     .build
 
-  sf.modifyVolume( modifyVolume )
+  // Send the modify volume request
+  sfe.modifyVolume( modifyVolume )
 }
