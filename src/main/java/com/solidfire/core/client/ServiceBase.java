cac5ac1381e5b057db788745843907a965e6f711
Adam Haid <adam.haid@solidfire.com>
2017-01-23 10:04:33 -0700

    Coverity defect fixes

--- src/main/java/com/solidfire/core/client/ServiceBase.java
+++ src/main/java/com/solidfire/core/client/ServiceBase.java
@@ -200,7 +200,6 @@ public class ServiceBase {
                 throw new ApiServerException("Not Found", "404", matcher.group(1));
             }
             // Removes the html tags from the response.
-            final Pattern htmlTag = Pattern.compile("<.*?>");
             response.replaceAll("<.*?>", "");
             throw new ApiException(format("There was a problem parsing the response from the server. ( response=%s )", response), e);
         } catch (NullPointerException | JsonParseException e) {
