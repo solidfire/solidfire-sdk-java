a9f88d864df2dd0b98e2bd81e1c08d16c6133408
Build Automation <dan.audette@solidfire.com>
2017-03-08 15:38:58 -0700

    Adding generated files

--- src/generated/java/com/solidfire/element/api/VolumeQOS.java
+++ src/generated/java/com/solidfire/element/api/VolumeQOS.java
@@ -34,9 +34,9 @@ import java.util.Objects;
 public class VolumeQOS implements Serializable {
 
     public static final long serialVersionUID = 677275932188527448L;
-    @SerializedName("minIOPS") private Long minIOPS;
-    @SerializedName("maxIOPS") private Long maxIOPS;
-    @SerializedName("burstIOPS") private Long burstIOPS;
+    @SerializedName("minIOPS") private Optional<Long> minIOPS;
+    @SerializedName("maxIOPS") private Optional<Long> maxIOPS;
+    @SerializedName("burstIOPS") private Optional<Long> burstIOPS;
     @SerializedName("burstTime") private Long burstTime;
     @SerializedName("curve") private java.util.Map<String,Long> curve;
 
@@ -48,16 +48,16 @@ public class VolumeQOS implements Serializable {
     // parameterized constructor
     @Since("7.0")
     public VolumeQOS(
-        Long minIOPS,
-        Long maxIOPS,
-        Long burstIOPS,
+        Optional<Long> minIOPS,
+        Optional<Long> maxIOPS,
+        Optional<Long> burstIOPS,
         Long burstTime,
         java.util.Map<String,Long> curve
     )
     {
-        this.minIOPS = minIOPS;
-        this.maxIOPS = maxIOPS;
-        this.burstIOPS = burstIOPS;
+        this.minIOPS = (minIOPS == null) ? Optional.<Long>empty() : minIOPS;
+        this.maxIOPS = (maxIOPS == null) ? Optional.<Long>empty() : maxIOPS;
+        this.burstIOPS = (burstIOPS == null) ? Optional.<Long>empty() : burstIOPS;
         this.burstTime = burstTime;
         this.curve = curve;
     }
@@ -67,24 +67,24 @@ public class VolumeQOS implements Serializable {
      * The allowed IOPS will only drop below this level if all volumes have been capped
      * at their min IOPS value and there is still insufficient performance capacity.
      **/
-    public Long getMinIOPS() { return this.minIOPS; }
-    public void setMinIOPS(Long minIOPS) { 
-        this.minIOPS = minIOPS;
+    public Optional<Long> getMinIOPS() { return this.minIOPS; }
+    public void setMinIOPS(Optional<Long> minIOPS) { 
+        this.minIOPS = (minIOPS == null) ? Optional.<Long>empty() : minIOPS;
     }
     /** 
      * Desired maximum 4KB IOPS allowed over an extended period of time.
      **/
-    public Long getMaxIOPS() { return this.maxIOPS; }
-    public void setMaxIOPS(Long maxIOPS) { 
-        this.maxIOPS = maxIOPS;
+    public Optional<Long> getMaxIOPS() { return this.maxIOPS; }
+    public void setMaxIOPS(Optional<Long> maxIOPS) { 
+        this.maxIOPS = (maxIOPS == null) ? Optional.<Long>empty() : maxIOPS;
     }
     /** 
      * Maximum "peak" 4KB IOPS allowed for short periods of time.
      * Allows for bursts of I/O activity over the normal max IOPS value.
      **/
-    public Long getBurstIOPS() { return this.burstIOPS; }
-    public void setBurstIOPS(Long burstIOPS) { 
-        this.burstIOPS = burstIOPS;
+    public Optional<Long> getBurstIOPS() { return this.burstIOPS; }
+    public void setBurstIOPS(Optional<Long> burstIOPS) { 
+        this.burstIOPS = (burstIOPS == null) ? Optional.<Long>empty() : burstIOPS;
     }
     /** 
      * The length of time burst IOPS is allowed.
@@ -142,9 +142,15 @@ public class VolumeQOS implements Serializable {
         final StringBuilder sb = new StringBuilder();
         sb.append( "{ " );
 
-        sb.append(" minIOPS : ").append(minIOPS).append(",");
-        sb.append(" maxIOPS : ").append(maxIOPS).append(",");
-        sb.append(" burstIOPS : ").append(burstIOPS).append(",");
+        if(null != minIOPS && minIOPS.isPresent()){
+            sb.append(" minIOPS : ").append(minIOPS).append(",");
+        }
+        if(null != maxIOPS && maxIOPS.isPresent()){
+            sb.append(" maxIOPS : ").append(maxIOPS).append(",");
+        }
+        if(null != burstIOPS && burstIOPS.isPresent()){
+            sb.append(" burstIOPS : ").append(burstIOPS).append(",");
+        }
         sb.append(" burstTime : ").append(burstTime).append(",");
         sb.append(" curve : ").append(curve).append(",");
         sb.append( " }" );
@@ -164,9 +170,9 @@ public class VolumeQOS implements Serializable {
     }
 
     public static class Builder {
-        private Long minIOPS;
-        private Long maxIOPS;
-        private Long burstIOPS;
+        private Optional<Long> minIOPS;
+        private Optional<Long> maxIOPS;
+        private Optional<Long> burstIOPS;
         private Long burstTime;
         private java.util.Map<String,Long> curve;
 
@@ -191,18 +197,18 @@ public class VolumeQOS implements Serializable {
             return this;
         }
 
-        public VolumeQOS.Builder minIOPS(final Long minIOPS) {
-            this.minIOPS = minIOPS;
+        public VolumeQOS.Builder optionalMinIOPS(final Long minIOPS) {
+            this.minIOPS = (minIOPS == null) ? Optional.<Long>empty() : Optional.of(minIOPS);
             return this;
         }
 
-        public VolumeQOS.Builder maxIOPS(final Long maxIOPS) {
-            this.maxIOPS = maxIOPS;
+        public VolumeQOS.Builder optionalMaxIOPS(final Long maxIOPS) {
+            this.maxIOPS = (maxIOPS == null) ? Optional.<Long>empty() : Optional.of(maxIOPS);
             return this;
         }
 
-        public VolumeQOS.Builder burstIOPS(final Long burstIOPS) {
-            this.burstIOPS = burstIOPS;
+        public VolumeQOS.Builder optionalBurstIOPS(final Long burstIOPS) {
+            this.burstIOPS = (burstIOPS == null) ? Optional.<Long>empty() : Optional.of(burstIOPS);
             return this;
         }
 
