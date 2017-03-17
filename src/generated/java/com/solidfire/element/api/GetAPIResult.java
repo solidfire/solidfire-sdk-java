6762e1dc5c3a7889da8d8a2e55c1bacc97618235
Build Automation <dan.audette@solidfire.com>
2017-03-17 11:59:01 -0600

    Adding generated files

--- src/generated/java/com/solidfire/element/api/GetAPIResult.java
+++ src/generated/java/com/solidfire/element/api/GetAPIResult.java
@@ -33,8 +33,8 @@ import java.util.Objects;
 public class GetAPIResult implements Serializable {
 
     public static final long serialVersionUID = -334735413474527149L;
-    @SerializedName("currentVersion") private String currentVersion;
-    @SerializedName("supportedVersions") private String[] supportedVersions;
+    @SerializedName("currentVersion") private Double currentVersion;
+    @SerializedName("supportedVersions") private Double[] supportedVersions;
 
     // empty constructor
     @Since("7.0")
@@ -44,8 +44,8 @@ public class GetAPIResult implements Serializable {
     // parameterized constructor
     @Since("7.0")
     public GetAPIResult(
-        String currentVersion,
-        String[] supportedVersions
+        Double currentVersion,
+        Double[] supportedVersions
     )
     {
         this.currentVersion = currentVersion;
@@ -55,15 +55,15 @@ public class GetAPIResult implements Serializable {
     /** 
      * 
      **/
-    public String getCurrentVersion() { return this.currentVersion; }
-    public void setCurrentVersion(String currentVersion) { 
+    public Double getCurrentVersion() { return this.currentVersion; }
+    public void setCurrentVersion(Double currentVersion) { 
         this.currentVersion = currentVersion;
     }
     /** 
      * 
      **/
-    public String[] getSupportedVersions() { return this.supportedVersions; }
-    public void setSupportedVersions(String[] supportedVersions) { 
+    public Double[] getSupportedVersions() { return this.supportedVersions; }
+    public void setSupportedVersions(Double[] supportedVersions) { 
         this.supportedVersions = supportedVersions;
     }
 
@@ -116,8 +116,8 @@ public class GetAPIResult implements Serializable {
     }
 
     public static class Builder {
-        private String currentVersion;
-        private String[] supportedVersions;
+        private Double currentVersion;
+        private Double[] supportedVersions;
 
         private Builder() { }
 
@@ -134,12 +134,12 @@ public class GetAPIResult implements Serializable {
             return this;
         }
 
-        public GetAPIResult.Builder currentVersion(final String currentVersion) {
+        public GetAPIResult.Builder currentVersion(final Double currentVersion) {
             this.currentVersion = currentVersion;
             return this;
         }
 
-        public GetAPIResult.Builder supportedVersions(final String[] supportedVersions) {
+        public GetAPIResult.Builder supportedVersions(final Double[] supportedVersions) {
             this.supportedVersions = supportedVersions;
             return this;
         }
